011e0bad16fc97f3beed150578b29ff0253e9b26
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-16 01:34:35 +0100

    making realtime application

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,19 +17,19 @@ class UserSeeder extends Seeder
         $number_of_users = 20;
         $user1 = User::factory()->create(['email' => 'dahmenhacker41@gmail.com']);
         $user2 = User::factory()->create(['email' => 'dahmenhacker12@gmail.com']);
-//        $user1->friends()->syncWithoutDetaching($user2);
-//        $user2->friends()->syncWithoutDetaching($user1);
-//        User::factory($number_of_users)->create();
-//        $users = User::all();
-        // adding friends to the users
-//        $users->each(function ($user) use ($number_of_users, $users) {
-//            $number_of_friends = rand(2, 5);
-//            for ($i = 0; $i < $number_of_friends; $i++) {
-//                $random_user = $users->whereNotIn('id', $user)->random();
-//                $user->friends()->syncWithoutDetaching($random_user);
-//                $random_user->friends()->syncWithoutDetaching($user);
-//            }
-//        });
+        $user1->friends()->syncWithoutDetaching($user2);
+        $user2->friends()->syncWithoutDetaching($user1);
+        User::factory($number_of_users)->create();
+        $users = User::all();
+//         adding friends to the users
+        $users->each(function ($user) use ($number_of_users, $users) {
+            $number_of_friends = rand(2, 5);
+            for ($i = 0; $i < $number_of_friends; $i++) {
+                $random_user = $users->whereNotIn('id', $user)->random();
+                $user->friends()->syncWithoutDetaching($random_user);
+                $random_user->friends()->syncWithoutDetaching($user);
+            }
+        });
 
     }
 }
