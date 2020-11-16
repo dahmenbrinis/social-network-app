bd923b84a365a9abc8f3f06da57ea1ccafc76952
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-16 14:37:22 +0100

    setting the friends request notification

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -23,8 +23,33 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
+
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <!-- Friend Requests Dropdown -->
+                <div class="mx-8">
+                    <x-jet-dropdown align="right" class="">
+                        <x-slot name="trigger">
+                            <button
+                                class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
+                                <svg class="h-6 w-6 text-indigo-500" viewBox="0 0 24 24" fill="none"
+                                     stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                     stroke-linejoin="round">
+                                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+                                    <circle cx="9" cy="7" r="4"/>
+                                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"/>
+                                    <path d="M16 3.13a4 4 0 0 1 0 7.75"/>
+                                </svg>
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+                            <div class="block px-4 py-2 text-sm ">
+                                No Notification
+                            </div>
+                        </x-slot>
+                    </x-jet-dropdown>
+                </div><!-- Settings Dropdown -->
+
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
