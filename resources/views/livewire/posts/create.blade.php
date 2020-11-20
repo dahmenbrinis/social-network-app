3fa10904e67ac6b156f284ed46ba586526871e9a
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-20 19:30:08 +0100

    adding the photo functionality of the post and some route cleanup

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -27,7 +27,16 @@
                 <input type='file' wire:model="images" class="hidden" multiple/>
             </label>
 
-            <button wire:click="save" type="submit" data-dismiss="modal" class="post-share-btn px-5 ">post
+            <button wire:click="save" wire:loading.remove wire:target="images" type="submit" data-dismiss="modal"
+                    class="post-share-btn px-5 ">post
+            </button>
+            <button wire:loading wire:target="images" type="button" class="post-share-btn px-5 " disabled>
+                <svg class="animate-spin h-3 w-3 mx-2 text-white " xmlns="http://www.w3.org/2000/svg" fill="none"
+                     viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
             </button>
         </div>
     </div>
