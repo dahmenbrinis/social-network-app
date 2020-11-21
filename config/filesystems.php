c30d7b83f9259ef0e1f44afc44175ea9ae91a579
Abderrahmen Brinis <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-21 14:02:37 +0100

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,8 +50,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path() . '/storage',
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path() . '/storagedrive',
+            'url' => env('APP_URL').'/storagedrive',
             'visibility' => 'public',
         ],
 
