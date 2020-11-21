ed7ec29cbc51435dcae3f50b53de135680250d04
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-21 14:31:00 +0100

    compressing the app.js file

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,12 +7,12 @@
 
 
 window._ = require('lodash');
-try {
-    window.Popper = require('popper.js').default;
-
-    window.$ = window.jQuery = require('jquery');
-} catch (e) {
-}
+// try {
+//     window.Popper = require('popper.js').default;
+//
+//     window.$ = window.jQuery = require('jquery');
+// } catch (e) {
+// }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
