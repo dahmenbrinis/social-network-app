e16f7f1a48bff3234f0a1b3621b56b20f2d06fc8
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-27 22:32:24 +0100

    breakpoint 30% progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @livewireStyles
 @include('layouts.styles')
