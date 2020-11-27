363c561f0a90cabb234756ac8d87bb4ac3188933
dahmenbriniss <72577453+dahmenbriniss@users.noreply.github.com>
2020-11-27 10:55:00 +0100

    chat functionality

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,9 +9,9 @@ import Echo from 'laravel-echo';
 
 window._ = require('lodash');
 try {
-    window.Popper = require('popper.js').default;
+    // window.Popper = require('popper.js').default;
     //
-    window.$ = window.jQuery = require('jquery');
+    // window.$ = window.jQuery = require('jquery');
 } catch (e) {
 }
 
